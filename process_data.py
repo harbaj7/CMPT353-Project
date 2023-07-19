e6fecf5cc622362f90bbdd08705e6698e4a6629d
itBeABruhMoment <reee271314@gmail.com>
2023-07-19 10:10:51 -0700

    more figrues

--- process_data.py
+++ process_data.py
@@ -23,7 +23,9 @@ def smallestAngle(data_orient):
         created.append(column + '_cos', column + '_sin')
     print(data_orient)
 
-    data_orient['vec_x'] = -data_orient['roll_sin'] * data_orient['yaw_sin'] - data_orient['roll_cos']
+    data_orient['vec_x'] = -data_orient['roll_sin'] * data_orient['yaw_cos'] - data_orient['roll_cos'] * data_orient['pitch_sin'] * data_orient['yaw_sin']
+    data_orient['vec_y'] = data_orient['roll_sin'] * data_orient['yaw_sin'] - data_orient['roll_cos'] * data_orient['pitch_sin'] * data_orient['yaw_sin']
+    data_orient['vec_z'] = data_orient['roll_cos'] * data_orient['pitch_cos']
 
 
 # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
@@ -75,6 +77,34 @@ for file in files:
     plt.plot(data_orient['seconds_elapsed'], ica[:,0], 'g-')
     plt.plot(data_orient['seconds_elapsed'], ica[:,1], 'b-')
     plt.savefig('figures/' + file.split('.')[0] + '_ica.png')
+
+    data_grav = pd.read_csv(ZipFile('data/' + file).open('Gravity.csv'))
+    data_grav = data_grav[data_grav['seconds_elapsed'] > 7]
+    data_grav = data_grav[data_grav['seconds_elapsed'] < 15]
+    vectors = data_grav[['x', 'y', 'z']].to_numpy()
+    dot = np.dot(vectors, np.array([1, 0, 0]))
+    length = np.linalg.norm(vectors, axis=1)
+    data_grav['length'] = length
+    angle = np.arccos(dot / length)
+    data_grav['angle'] = angle
+    # data_grav = data_grav.sort_values('angle')
+    print(data_grav)
+    
+    plt.figure(figsize=(16, 16))
+    plt.title('grav angle')
+    plt.plot(data_grav['seconds_elapsed'], data_grav['angle'], 'r-')
+    plt.savefig('figures/' + file.split('.')[0] + '_grav_angle.png')
+
+    plt.figure(figsize=(16, 16))
+    plt.title('grav')
+    plt.plot(data_grav['seconds_elapsed'], data_grav['x'], 'r-')
+    plt.plot(data_grav['seconds_elapsed'], data_grav['y'], 'g-')
+    plt.plot(data_grav['seconds_elapsed'], data_grav['z'], 'b-')
+    plt.plot(data_grav['seconds_elapsed'], data_grav['angle'], 'y-')
+    plt.savefig('figures/' + file.split('.')[0] + '_grav.png')
+
+    # to get down, try calculating all angles with straight down, sorting by that, nad averaging all the vectors
+    # break
 
     
 
