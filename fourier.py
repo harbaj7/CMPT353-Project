b04fa5574228052a4e3a14962230c3a20625ed6b
harbaj <hsg18@sfu.ca>
2023-08-02 21:01:43 -0700

    refined the code and plots

--- fourier.py
+++ fourier.py
@@ -1,8 +1,8 @@
 # does a fft on the grouped gravity vector data
 import re
 import os
-from os import listdir
-from os.path import isfile, join
+from os import listdir, makedirs
+from os.path import isfile, join, exists
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
@@ -16,15 +16,15 @@ NUM_SECS_DISPLAY = 10
 BUFFER_SECS = 5
 RENAME = {'gravity_x':'x', 'gravity_y':'y', 'gravity_z':'z', 'seconds_elapsed_rounded':'seconds_elapsed'}
 
-
-########################################################################- Harbaj code update
+# Create the 'grouped' directory if it doesn't exist
+if not exists('fft'):
+    makedirs('fft')
 data_list = []
 def dominant_frequency(frequencies, magnitudes):
     # Find the index of the peak
     peak_index = np.argmax(magnitudes)
     # Return the corresponding frequency
     return frequencies[peak_index]
-
 
 def fourier(data, col):
     N = data.shape[0]
@@ -58,7 +58,6 @@ for file in files:
     frequency_y, fft_y, minus_mean_y = fourier(data, 'y')
     frequency_z, fft_z, minus_mean_z = fourier(data, 'z')
 
-    ################################ - Harbaj code update - 
     # Find the dominant frequencies
     dominant_frequency_x = dominant_frequency(frequency_x, fft_x)
     dominant_frequency_y = dominant_frequency(frequency_y, fft_y)
@@ -81,25 +80,33 @@ for file in files:
         'dominant_frequency_z': dominant_frequency_z,
         'activity': activity
     })
-    
-    ###############################
-    
-    # plot
-    fig, axs = plt.subplots(2, 2, figsize=(15, 15))
-    axs[0, 0].set_title('fft')
-    axs[0, 0].plot(frequency_x, fft_x, 'r-')
-    axs[0, 0].plot(frequency_y, fft_y, 'g-')
-    axs[0, 0].plot(frequency_z, fft_z, 'b-')
-    axs[0, 1].set_title('data - mean')
-    axs[0, 1].plot(data['seconds_elapsed'], minus_mean_x, 'r-')
-    axs[0, 1].plot(data['seconds_elapsed'], minus_mean_y, 'g-')
-    axs[0, 1].plot(data['seconds_elapsed'], minus_mean_z, 'b-')
+     
+    fig, axs = plt.subplots(1, 2, figsize=(15, 7))  # Adjust subplot arrangement and figure size
+
+    # Plotting FFT
+    axs[0].set_title('FFT')
+    axs[0].plot(frequency_x, fft_x, 'r-', label='x')
+    axs[0].plot(frequency_y, fft_y, 'g-', label='y')
+    axs[0].plot(frequency_z, fft_z, 'b-', label='z')
+    axs[0].set_xlabel('Frequency (Hz)')  # x-axis label
+    axs[0].set_ylabel('Amplitude')  # y-axis label
+    axs[0].legend()  # Legend
+
+    # Plotting data - mean
+    axs[1].set_title('Data - Mean')
+    axs[1].plot(data['seconds_elapsed'], minus_mean_x, 'r-', label='x')
+    axs[1].plot(data['seconds_elapsed'], minus_mean_y, 'g-', label='y')
+    axs[1].plot(data['seconds_elapsed'], minus_mean_z, 'b-', label='z')
+    axs[1].set_xlabel('Time (s)')  # x-axis label
+    axs[1].set_ylabel('Amplitude')  # y-axis label
+    axs[1].legend()  # Legend
+
+    plt.tight_layout()  # Improve layout
 
     plt.savefig('fft/' + file.split('.')[0] + '_fft.png')
     plt.close()
     # break
     
-################################################################ - Harbaj code update Analysis
 df = pd.DataFrame(data_list)
 
 # Check if the directory exists
@@ -107,7 +114,7 @@ if not os.path.exists('analysis'):
     # If not, create it
     os.makedirs('analysis')
 
-# Then, you can save the DataFrame
+# save the DataFrame
 df.to_csv('analysis/activity_analysis.csv', index=False)
 
 
